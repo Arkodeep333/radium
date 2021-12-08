bcb49c4d4c1f216ecff1569ad90566d444828aef
Arkodeep333 <arkojaihind@gmail.com>
2021-12-08 22:38:11 +0530

    project 2 completed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-
 const route = require('./routes/route.js');
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const mongoose = require('mongoose');
+const { response } = require('express');
+
+mongoose.connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/Arkodeep_Chakraborty?retryWrites=true&w=majority", { useNewUrlParser: true })
+    .then(() => console.log('mongodb running and connected'))
+    .catch(err => console.log(err))
+
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-	console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
